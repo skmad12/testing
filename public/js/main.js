6c0665bea49b97628d5c97ff788eb838b6c4c95a
mi-yu <m.yu8088@gmail.com>
2016-01-29 22:05:01 -0600

    options work, added small border radius to img, marked up css

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 	var numCorrect = 0;
 	var total = 0;
-	var currFolder = "Anoplophora_glabripennis" /*""*/;
 	var currIndex = 0;
+	var acceptScientificNames = false;
+	var acceptCommonNames = false;
 	var folderNames = ["Diaphorina_citri","Anoplophora_glabripennis","Aedes_albopictus","Halyomorpha_halys","Cactoblastis_cactorum","Scirtothrips_dorsalis","Anoplophora_chinensis","Tomicus_piniperda","Agrilus_planipennis","Lymantria_dispar","Ips_typographus","Coptotermes_formosanus","Lissachatina_fulica","Homalodisca_vitripennis","Adelges_tsugae","Epiphyas_postvittana","Ceratitis_capitata","Anastrepha_ludens","Pectinophora_gossypiella","Maconellicoccus_hirsutus","Solenopsis_invicta","Diuraphis_noxia","Bemisia_argentifolii","Heterodera_glycines","Alosa_pseudoharengus","Asian_carp","Corbicula_fluminea","Hemigrapsus_sanguineus","Monopterus_albus","Eriocheir_sinensis","Styela_clava","Gymnocephalus_cernuus","Carcinus_maenas","Pterois_volitans","Channa_argus","Potamopyrgus_antipodarum","Myocastor_coypus","Dreissena_bugensis","Neogobius_melanostomus","Orconectes_rusticus","Petromyzon_marinus","Didemnum_vexillum","Bythotrephes_longimanus","Rapana_venosa","Phyllorhiza_punctata","Dreissena_polymorpha","Dioscorea_bulbifera","Elaeagnus_umbellata","Vitex_rotundifolia","Schinus_terebinthifolius","Cirsium_arvense","Triadica_sebifera","Imperata_cylindrica","Rhamnus_cathartica","Dipsacus_fullonum","Linaria_dalmatica","Centaurea_diffusa","Bromus_tectorum","Ficaria_verna","Alliaria_petiolata","Heracleum_mantegazzianum","Lepidium_appelianum","Cynoglossum_officinale","Lygodium_japonicum","Lonicera_japonica","Fallopia_japonica","Spiraea_japonica","Microstegium_vimineum","Sorghum_halepense","Pueraria_montana_var._lobata","Euphorbia_esula","Taeniatherum_caput-medusae","Persicaria_perfoliata","Rosa_multiflora","Carduus_nutans","Lygodium_microphyllum","Paulownia_tomentosa","Centaurea_calcitrapa","Rhaponticum_repens","Elaeagnus_angustifolia","Tamarix_spp","Hypericum_perforatum","Cytisus_scoparius","Onopordum_acanthium","Centaurea_stoebe","Ailanthus_altissima","Solanum_viarum","Lepidium_draba","Striga_asiatica","Centaurea_solstitialis","Linaria_vulgaris","Alternanthera_philoxeroides","Egeria_densa","Caulerpa_taxifolia","Phragmites_australis","Potamogeton_crispus","Didymosphenia_geminata","Myriophyllum_spicatum","Arundo_donax","Salvinia_molesta","Hydrilla_verticillata","Melaleuca_quinquenervia","Lythrum_salicaria","Eichhornia_crassipes","Pistia_stratiotes","Ipomoea_aquatica","Ophiostoma","Sirococcus_clavigignenti-juglandacearum","Ceratocystis_fagacearum","Pseudogymnoascus_destructans","Orbivirus","Flavivirus","H5N5"];
+	var speciesNames = ["Asian_Citrus_Psyllid","Asian_Long-Horned_Beetle","Asian_Tiger_Mosquito","Brown_Marmorated_Stink_Bug","Cactus_Moth","Chilli_Thrips","Citrus_Longhorned_Beetle","Common_Pine_Shoot_Beetle","Emerald_Ash_Borer","European_Gypsy_Moth","European_Spruce_Bark_Beetle","Formosan_Subterranean_Termite","Giant_African_Snail","Glassy-Winged_Sharpshooter","Hemlock_Woolly_Adelgid","Light_Brown_Apple_Moth","Mediterranean_Fruit_Fly","Mexican_Fruit_Fly","Pink_Bollworm","Pink_Hibiscus_Mealybug","Red_Imported_Fire_Ant","Russian_Wheat_Aphid","Silverleaf_Whitefly","Soybean_Cyst_Nematode","Alewife","Asian_Carps","Asian_Clam","Asian_Shore_Crab","Asian_Swamp_Eel","Chinese_Mitten_Crab","Clubbed_Tunicate","Eurasian_Ruffe","European_Green_Crab","Lionfish","Northern_Snakehead","New_Zealand_Mud_Snail","Nutria","Quagga_Mussel","Round_Goby","Rusty_Crayfish","Sea_Lamprey","Sea_Squirt","Spiny_Water_Flea","Veined_Rapa_Whelk","White_Spotted_Jellyfish","Zebra_Mussel","Air_Potato","Autumn_Olive","Beach_Vitex","Brazilian_Peppertree","Canada_Thistle","Chinese_Tallow","Cogongrass","Common_Buckthorn","Common_Teasel","Dalmatian_Toadflax","Diffuse_Knapweed","Downy_Brome","Fig_Buttercup","Garlic_Mustard","Giant_Hogweed","Hairy_Whitetop","Houndstongue","Japanese_Climbing_Fern","Japanese_Honeysuckle","Japanese_Knotweed","Japanese_Spiraea","Japanese_Stilt_Grass","Johnsongrass","Kudzu","Leafy_Spurge","Medusahead","Mile-A-Minute_Weed","Multiflora_Rose","Musk_Thistle","Old_World_Climbing_Fern","Princess_Tree","Purple_Star_Thistle","Russian_Knapweed","Russian_Olive","Saltcedar","St._Johnswort","Scotch_Broom","Scotch_Thistle","Spotted_Knapweed","Tree-of-Heaven","Tropical_Soda_Apple","Whitetop","Witchweed","Yellow_Star_Thistle","Yellow_Toadflax","Alligatorweed","Brazilian_Waterweed","Caulerpa,_Mediterranean_Clone","Common_Reed","Curly_Pondweed","Rock_Snot","Eurasian_Watermilfoil","Giant_Reed","Giant_Salvinia","Hydrilla","Melaleuca","Purple_Loosestrife","Water_Hyacinth","Water_Lettuce","Water_Spinach","Dutch_Elm_Disease","Butternut_Canker","Oak_Wilt","Whitenose_Bat_Syndrome","Blue_Tongue_Virus","West_Nile_Virus","Bird_Flu"];
+	var currFolder = folderNames[1];
+	var currCommonName = speciesNames[1];
+
 	var getNewImg = function() {
 		var tempIndex = Math.floor(Math.random()*113);
 		//currFolder = folderNames[tempIndex];
@@ -12,8 +17,11 @@ $(document).ready(function() {
 	}
 	var result = $("#result");
 	var validate = function() {
-		var input = $(".form-control").val().trim().replace(" ","_").toLowerCase();
-		if(input===/*"regina is difficult"*/currFolder.toLowerCase()) {
+		var input = $(".form-control").val().trim().replace(/ /g, '_').toLowerCase();
+		//console.log("matches sci name "+(acceptScientificNames&&input===currFolder.toLowerCase()));
+		console.log("accepting common names "+acceptCommonNames);
+		console.log("input vs ans "+input+" "+currCommonName.toLowerCase());
+		if((acceptScientificNames&&input===currFolder.toLowerCase())||(acceptCommonNames && input===currCommonName.toLowerCase())) {
 			result.text("Correct!");
 			result.removeClass("alert-danger").addClass("alert-success");
 			result.removeClass("hidden");
@@ -32,7 +40,7 @@ $(document).ready(function() {
 		}
 	};
 	var showAns = function() {
-		$("#answer").text("Correct answer: "+currFolder.replace("_"," "));
+		$("#answer").text(currCommonName.replace(/\_/g, ' ')+"  /  "+currFolder.replace(/\_/g, ' '));
 	}
 
 	var incrementCount = function(correct) {
@@ -50,7 +58,14 @@ $(document).ready(function() {
 
 	$("#start-game").click( function() {
 		$(this).hide();
+		$("#options").hide();
 		$("#image-container").removeClass("hidden");
+		if ($("#sci-names").prop('checked')) {
+			acceptScientificNames = true;
+		}
+		if ($("#common-names").prop('checked')) {
+			acceptCommonNames = true;
+		}
 		getNewImg();
 	})
 	$("#submit").click(validate);
